f2f1070448b8257bf09f8546c142148c5afc8894
anthager <anthage@student.chalmers.se>
2019-04-02 12:48:34 +0200

    Redo it with native promises and changed name

--- script.ts
+++ script.ts
@@ -1,44 +1,26 @@
 import fetch from "node-fetch";
 import { Day } from "./interfaces";
-import { from, forkJoin } from "rxjs";
-import { map, switchAll, filter, reduce } from "rxjs/operators";
+import * as _ from "lodash";
 
 (async () => {
-  // const meals = {};
-  from<Promise<Day[]>>(
-    (await fetch(
-      "http://carbonateapiprod.azurewebsites.net/api/v1/mealprovidingunits/871c63d7-4ddb-46b8-d2a0-08d558129279/dishoccurrences?startDate=2017-04-04&endDate=2019-10-05"
-    )).json()
-  )
-    .pipe(
-      switchAll(),
-      map(day =>
-        forkJoin(
-          from(day.displayNames).pipe(
-            filter(
-              displayName =>
-                displayName.displayNameCategory.displayNameCategoryName ===
-                "Swedish"
-            ),
-            map(displayName => displayName.dishDisplayName)
-          )
+  const meals: Record<string, number> = {};
+  const days: Day[] = await (await fetch(
+    "http://carbonateapiprod.azurewebsites.net/api/v1/mealprovidingunits/3d519481-1667-4cad-d2a3-08d558129279/dishoccurrences?startDate=2019-04-01&endDate=2019-04-01"
+  )).json();
+  const foods = _.flatMapDeep(
+    days.map(day =>
+      day.displayNames
+        .filter(
+          dn => dn.displayNameCategory.displayNameCategoryName === "Swedish"
         )
-      ),
-      switchAll(),
-      map(a => a[0].trim())
+        .map(dn => dn.dishDisplayName.trim())
     )
-    .subscribe(a => console.log(a));
-  // days.forEach(day => day.length && console.log("nice"));
-  // const lengths = days.map(day => day.displayNames.length);
-  // days.forEach(day => {
-  //   day.displayNames.map;
-  // });
-  // days.map(day=> day.displayNames.map(display => display.dishDisplayName)).reduce((acc:Map<string, number> , curr): Map<string, number> => {
-  // 	acc.get('curr')
-  // }, new Map<string, number>);
-  // console.log(days[1].displayNames.map(name => name.id));
-  // const a = [1, 2, 3];
-  // from(a).subscribe(arr => {
-  //   console.log(arr);
-  // });
+  );
+  foods.forEach(food => (meals[food] ? meals[food]++ : (meals[food] = 1)));
+  const avg =
+    Object.values(meals).reduce((acc, curr) => acc + curr, 0) /
+    Object.keys(meals).length;
+  console.log(meals);
+  console.log(avg);
+  console.log(Object.keys(meals).length);
 })();
