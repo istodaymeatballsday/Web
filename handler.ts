f863b04ef021463eda57407482a0f9f438e9d0cc
anthager <anthage@student.chalmers.se>
2019-04-08 16:39:01 +0200

    add the site

--- handler.ts
+++ handler.ts
@@ -1,13 +1,44 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
-// import { ApolloServer, gql } from "apollo-server-lambda";
 
-export const hello: APIGatewayProxyHandler = async (event, _context) => {
+export const meatballs: APIGatewayProxyHandler = async () => {
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message:
-        "Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!",
-      input: event
-    })
+    headers: {
+      "Content-Type": "text/html"
+    },
+    body: `<!-- saved from url=(0042)http://istodaymacproday.com/?promocode=apt -->
+    <html class="gr__istodaymacproday_com">
+      <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=windows-1252" />
+        <title>Is today Meatballs day?</title>
+        <meta name="theme-color" content="#ffffff" />
+        <style>
+          body {
+            font-family: -apple-system, BlinkMacSystemFont, sans-serif, Roboto,
+              Ubuntu, "Segoe UI";
+            text-align: center;
+            font-size: 100px;
+            color: #222222;
+          }
+        </style>
+      </head>
+      <body data-gr-c-s-loaded="true">
+        <div align="center" id="answer" style="font-size: 100px;">
+          <br />
+          <h1>Nope.</h1>
+        </div>
+        <script>
+          var queryFields = window.location.search
+            .substring(1)
+            .split("&")[0]
+            .split("=");
+          if (queryFields[0] == "promocode" && queryFields[1] == "atp") {
+            var theAnswer = document.getElementById("answer");
+            theAnswer.style.fontSize = "90%"; // 10% off is a good deal!
+          }
+        </script>
+      </body>
+    </html>
+    `
   };
 };
