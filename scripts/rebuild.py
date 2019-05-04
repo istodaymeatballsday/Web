c4a16f822760990f63d6e580801d7c48b6221fb5
anthager <anthage@student.chalmers.se>
2019-05-04 19:27:29 +0200

    update paths

--- scripts/rebuild.py
+++ scripts/rebuild.py
@@ -2,17 +2,18 @@
 import json
 import urllib2
 import time
+import sys
 
 tries = 10
 while tries >= 0:
 	try:
 		res = urllib2.urlopen('https://api.istodaymeatballsday.com').read()
-		f = open('/var/www/istodaymeatballsday.com/res.json', 'w+').write(res)
+		f = open('/istodaymeatballsday.com/build/res.json', 'w+').write(res)
 
 		answer = json.loads(res)['msg']
-		f = open('~/Web/index.template.html', 'r').read()
+		f = open('/istodaymeatballsday.com/Web/index.template.html', 'r').read()
 		new_html = f.replace('_ANSWER_', answer)
-		f = open('/var/www/istodaymeatballsday.com/index.html', 'w+').write(new_html)
+		f = open('/istodaymeatballsday.com/build/index.html', 'w+').write(new_html)
 		break
 	except Exception as e:
 		tries -= 1
