9e582568a70e2dd1e407be209b9fa8fb5c92bf4f
anthager <anthage@student.chalmers.se>
2019-05-12 19:19:54 +0200

    handle weekeds

--- scripts/rebuild.py
+++ scripts/rebuild.py
@@ -12,53 +12,59 @@ import os
 
 dev = True if 'ENV' in os.environ and os.environ['ENV'] == 'dev' else False
 
+
 def get_todays_food(start_date):
-		res = json.loads(urllib2.urlopen(
-				'http://carbonateapiprod.azurewebsites.net/api/v1/mealprovidingunits/' +
-				'3d519481-1667-4cad-d2a3-08d558129279/dishoccurrences' +
-				'?startDate=' + start_date).read())
-		return res
+    res = json.loads(urllib2.urlopen(
+        'http://carbonateapiprod.azurewebsites.net/api/v1/mealprovidingunits/' +
+        '3d519481-1667-4cad-d2a3-08d558129279/dishoccurrences' +
+        '?startDate=' + start_date).read())
+    return res
 
 
 def get_today():
-		return (datetime.utcnow() + timedelta(hours=2)).strftime('%Y-%m-%d')
+    return (datetime.utcnow() + timedelta(hours=2)).strftime('%Y-%m-%d')
 
 
 def parse_dishes(raw):
-		return raw[0]['displayNames'][1]['dishDisplayName'], raw[1]['displayNames'][1]['dishDisplayName']
+    if (len(raw)):
+        return raw[0]['displayNames'][1]['dishDisplayName'], raw[1]['displayNames'][1]['dishDisplayName']
+    return None, None
 
 
 def is_vegan(raw_dish):
-		return re.match('vegan', raw_dish['dishType']['dishTypeName'], re.IGNORECASE)
+    return re.match('vegan', raw_dish['dishType']['dishTypeName'], re.IGNORECASE)
 
 
 def write_to_res(meat, vegan, ans, code):
-		content = {'meat': meat, 'veg': vegan, 'msg': ans, 'code': code}
-		f = open('/istodaymeatballsday.com/build/res.json' if not dev else 'res.json', 'w+').write(json.dumps(content))
+    content = {'meat': meat, 'veg': vegan, 'msg': ans, 'code': code}
+    f = open('/istodaymeatballsday.com/build/res.json' if not dev else 'res.json',
+             'w+').write(json.dumps(content))
 
 
 def write_to_html(ans):
-		f = open('/istodaymeatballsday.com/Web/index.template.html' if not dev else 'index.template.html', 'r').read()
-		new_html = f.replace('_ANSWER_', ans)
-		f = open('/istodaymeatballsday.com/build/index.html' if not dev else 'index.html', 'w+').write(new_html)
+    f = open('/istodaymeatballsday.com/Web/index.template.html' if not dev else 'index.template.html', 'r').read()
+    new_html = f.replace('_ANSWER_', ans)
+    f = open('/istodaymeatballsday.com/build/index.html' if not dev else 'index.html',
+             'w+').write(new_html)
 
 
 def main():
-		while True:
-				try:
-						today = get_today()
-						raw = get_todays_food(today)
-						meat, vegan = parse_dishes(raw)
-						ans, code = ('Yep.', 1) if re.search(
-								'meatballs.*mashed|mashed.*meatballs', meat, re.IGNORECASE) else ('Nope.', 0)
-						write_to_res(meat, vegan, ans, code)
-						write_to_html(ans)
-						print datetime.now().__str__() + ': ' + ans + ' For date: ' + today
-						break
-				except Exception as e:
-						print datetime.now().__str__() + ': Error, tries again in 10 sec'
-						print e
-						time.sleep(10)
+    while True:
+        try:
+            today = get_today()
+            raw = get_todays_food(today)
+            meat, vegan = parse_dishes(raw)
+            ans, code = ('Yep.', 1) if meat and vegan and re.search(
+                'meatballs.*mashed|mashed.*meatballs', meat, re.IGNORECASE) else ('Nope.', 0)
+
+            write_to_res(meat, vegan, ans, code)
+            write_to_html(ans)
+            print datetime.now().__str__() + ': ' + ans + ' For date: ' + today
+            break
+        except Exception as e:
+            print datetime.now().__str__() + ': Error, tries again in 10 sec'
+            print e
+            time.sleep(10)
+
 
 main()
-
